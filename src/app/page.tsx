f1ff6eabc3fdc9db1b8ccddf1eb65f60b5a591b1
Maksym Matviichuk <maksymmatviichuk@Maksyms-MacBook-Pro.local>
2024-06-02 20:05:34 +0300

    signed out message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,40 @@
 /* eslint-disable @next/next/no-img-element */
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+const Images = async () => {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={`${image.id}-${index}`} className="flex w-48 flex-col">
+          <Link href={`/image/${image.id}`}>
+            <img src={image.url} alt="" className="h-48 w-full object-cover" />
+            <div className="">{image.name}</div>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default function HomePage() {
+  return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={`${image.id}-${index}`} className="flex w-48 flex-col">
-            <Link href={`/image/${image.id}`}>
-              <img
-                src={image.url}
-                alt=""
-                className="h-48 w-full object-cover"
-              />
-              <div className="">{image.name}</div>
-            </Link>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="p-4 text-center">
+          <p>You need to be signed in to view this page.</p>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
